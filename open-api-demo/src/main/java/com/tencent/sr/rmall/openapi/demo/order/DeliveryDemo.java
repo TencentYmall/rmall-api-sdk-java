1d9f058ffd010cf3c476ba09ff93521eae0b2831
stevenscao <stevenscao@tencent.com>
2020-08-11 17:52:30 +0800

    更改配置
    (cherry picked from commit 00e1122cc002098a12e051120931750ab21f88cb)

--- open-api-demo/src/main/java/com/tencent/sr/rmall/openapi/demo/order/DeliveryDemo.java
+++ open-api-demo/src/main/java/com/tencent/sr/rmall/openapi/demo/order/DeliveryDemo.java
@@ -19,17 +19,19 @@ public class DeliveryDemo {
 
     public static void main(String[] args) throws TsrSdkException {
         //测试环境传qa标识,不传默认为online
-        InitUtil.initSecret();
+        InitUtil.initSecret("qa");
         //订单sdk客户端  可以对客户端再做个性化处理
         //例如 设置超时时间, 个性化密钥
         TsrOrderClient orderClient = new TsrOrderClient();
-
-
+        //可根据需要设置客户端超时时间,默认连接3秒
+        orderClient.setConnectionTimeout(3000);
+        orderClient.setReadTimeout(3000);
         // 发货
         finishDeliveryDemo(orderClient);
     }
 
     public static void finishDeliveryDemo(TsrOrderClient orderClient) throws TsrSdkException {
+        //构建调用参数
         OpenApiFinishDeliveryRequest deliveryRequest = new OpenApiFinishDeliveryRequest();
         OpenApiFinishDeliveryRequest.DeliveryParameter parameter = new OpenApiFinishDeliveryRequest.DeliveryParameter();
         deliveryRequest.setParameter(parameter);
@@ -41,8 +43,8 @@ public class DeliveryDemo {
         logisticsInfoVO.setLogisticsCompanyCode("zhongtong");
         logisticsInfoVO.setLogisticsCompanyName("中通快递");
 
+        //发起远程调用
         HttpResult<Boolean> response = orderClient.finishDelivery(deliveryRequest);
-
         System.out.println("finishDeliveryDemo response: " + response);
     }
 }
